2aa2d2df178e36f9a868dedb263ec727e78ccdc6
Sophia Chisiya <totalsophie@gmail.com>
2023-11-26 16:51:48 +0300

    Add Hero Squad

---
+++ src/main/java/org/example/Hero.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Hero {
+    String name;
+    int age;
+    String specialPower;
+    String weakness;
+
+    public Hero(String name, int age, String specialPower, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+    }
+}
