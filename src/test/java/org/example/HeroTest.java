bce887bef99c8dae25e66643722b9b6737af0830
Sophia Chisiya <totalsophie@gmail.com>
2023-11-26 21:32:36 +0300

    Add hero test

---
+++ src/test/java/org/example/HeroTest.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HeroTest {
+    @Test
+    void testHeroJoinSquad() {
+        Hero hero = new Hero("TestHero", 25, "TestPower", "TestWeakness");
+        Squad squad = new Squad("TestSquad", 3, "TestCause");
+
+        hero.joinSquad(squad);
+
+        assertEquals(squad, hero.getSquad());
+        assertTrue(squad.getHeroes().contains(hero));
+    }
+
+    @Test
+    void testHeroLeaveSquad() {
+        Hero hero = new Hero("TestHero", 25, "TestPower", "TestWeakness");
+        Squad squad = new Squad("TestSquad", 3, "TestCause");
+
+        hero.joinSquad(squad);
+        hero.leaveSquad();
+
+        assertNull(hero.getSquad());
+        assertFalse(squad.getHeroes().contains(hero));
+    }
+
+
+}
