42820c1882004bdb0d2a306e367a0441c6dbf880
Sophia Chisiya <totalsophie@gmail.com>
2023-11-26 17:35:54 +0300

    Add getters on Hero

--- src/main/java/org/example/Hero.java
+++ src/main/java/org/example/Hero.java
@@ -5,11 +5,28 @@ public class Hero {
     int age;
     String specialPower;
     String weakness;
+    private Squad squad;
 
     public Hero(String name, int age, String specialPower, String weakness) {
         this.name = name;
         this.age = age;
         this.specialPower = specialPower;
         this.weakness = weakness;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
     }
 }
